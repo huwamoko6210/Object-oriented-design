47a408635fa54fccfee5104f3b76d69ab1ffb4be
gutty <ymgcmsts60@gmail.com>
2019-12-25 08:37:57 +0900

    first commit

---
+++ src/Inventory.java
@@ -0,0 +1,23 @@
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Inventory {
+    private List guitars;
+
+    public Inventory(){
+        guitars = new LinkedList();
+    }
+
+    public void addGuitar(String serialNumber, double price, String builder, String model, String type, String backWood, String topWood){
+        Guitar guitar = new Guitar(serialNumber, price, builder, model, type, backWood, topWood);
+        guitars.add(guitar);
+    }
+
+    public Guitar getGuitar(String serialNumber){
+        for(Iterator i = guitars.iterator(); i.hasNext();){
+            Guitar guitar = (Guitar)i.next();
+
+        }
+    }
+}
